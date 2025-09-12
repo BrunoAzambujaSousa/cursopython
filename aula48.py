8ce96ce95ba96767055ca584d2c89ce4e235edb9
Bruno Azambuja Nobre de Sousa <brunoazambuja@outlook.com>
2025-09-12 20:33:54 -0300

    Tipo list - Introdução às listas mutáveis em Python.

---
+++ aula48.py
@@ -0,0 +1,20 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+
+# nome = 'ABCDE' # 5 caracteres (len)
+# print(nome[2])
+
+
+# print(bool(lista)) # False
+# print(lista, type(lista))
+
+lista = [123, True, 'Bruno Azambuja', 1.5, []]
+lista[2] = 'Mauricio'
+print(lista)
+print(lista[2], type(lista[2]))
+
