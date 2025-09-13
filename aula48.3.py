b768ae83227a2b78ca203eb09ac6f7801bd75295
Bruno Azambuja Nobre de Sousa <brunoazambuja@outlook.com>
2025-09-12 21:18:07 -0300

    Inserindo itens em qualquer índice da lista com insert (tipo list).

---
+++ aula48.3.py
@@ -0,0 +1,27 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append - Adiciona um item ao final
+    insert - Adiciona um item no índice escolhido
+    pop - Remove do final ou do índice escolhido
+    del - apaga um índice
+    clear - limpa a lista
+    extend - estende a lista
+    + - concatena listas
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+
+lista = [10, 20, 30, 40]
+lista.append('Bruno Azambuja')
+print(lista)
+nome = lista.pop()
+lista.append(1234)
+del lista[-1]
+lista.insert(-1, 1914)
+print(lista)
+
+ 
